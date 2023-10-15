16d5bbc38bdb482b7852b88f23583fff5167aaa4
Enock Jeremi <enockjeremi@gmail.com>
2023-10-15 18:04:58 -0300

    fix to deploy 2

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,31 +1,33 @@
+const VERSION = process.env.NEXT_PUBLIC_VERSION_API;
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const endPoints = {
   reports: {
-    get: `${API_URL}/reports/`,
+    get: `${API_URL}/${VERSION}/reports/`,
 
     pagination: (limit: number | undefined, offset: number | undefined) =>
-      `${API_URL}/reports?limit=${limit}&offset=${offset}`,
+      `${API_URL}/${VERSION}/reports?limit=${limit}&offset=${offset}`,
 
-    create: `${API_URL}/reports/`,
-    getById: (id: number) => `${API_URL}/reports/${id}`,
-    updateById: (id: number | undefined) => `${API_URL}/reports/${Number(id)}`,
-    deleteById: (id: number) => `${API_URL}/reports/${id}`,
+    create: `${API_URL}/${VERSION}/reports/`,
+    getById: (id: number) => `${API_URL}/${VERSION}/reports/${id}`,
+    updateById: (id: number | undefined) =>
+      `${API_URL}/${VERSION}/reports/${Number(id)}`,
+    deleteById: (id: number) => `${API_URL}/${VERSION}/reports/${id}`,
 
     filterByCategoryName: (
       category: string | undefined,
       limit: number | undefined,
       offset: number | undefined
     ) =>
-      `${API_URL}/reports/filter-by-category?category=${category}&limit=${limit}&offset=${offset}`,
+      `${API_URL}/${VERSION}/reports/filter-by-category?category=${category}&limit=${limit}&offset=${offset}`,
 
     filterBy: (filter: string, limit?: number, offset?: number) =>
-      `${API_URL}/reports/filter-by-reports?filter=${filter}&limit=${limit}&offset=${offset}`,
+      `${API_URL}/${VERSION}/reports/filter-by-reports?filter=${filter}&limit=${limit}&offset=${offset}`,
   },
   categories: {
-    get: `${API_URL}/categories/`,
+    get: `${API_URL}/${VERSION}/categories/`,
     getByCategoryName: (category: string) =>
-      `${API_URL}/categories?category=${category}`,
-    getById: (id: number) => `${API_URL}/categories/${id}`,
+      `${API_URL}/${VERSION}/categories?category=${category}`,
+    getById: (id: number) => `${API_URL}/${VERSION}/categories/${id}`,
   },
 };
