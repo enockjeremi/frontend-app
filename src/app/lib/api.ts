11aefca7bd487c5c789e0af5f813350f7f2473e8
Enock Jeremi <enockjeremi@gmail.com>
2023-10-15 18:00:21 -0300

    fix to deploy

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,8 +1,4 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const TAG_REVALIDATE_URL = process.env.NEXT_PUBLIC_REVALIDATE_TAG_URL;
-const TAG = process.env.NEXT_PUBLIC_REVALIDATE_TAG;
-const PATCH_REVALIDATE_URL = process.env.NEXT_PUBLIC_REVALIDATE_PATH_URL;
-const TOKEN = process.env.NEXT_PUBLIC_MY_SECRET_TOKEN;
 
 export const endPoints = {
   reports: {
@@ -32,6 +28,4 @@ export const endPoints = {
       `${API_URL}/categories?category=${category}`,
     getById: (id: number) => `${API_URL}/categories/${id}`,
   },
-  revalidate_tag: `${TAG_REVALIDATE_URL}?tag=${TAG}&secret=${TOKEN}`,
-  revalidate_path: (PATH: string) => `${PATCH_REVALIDATE_URL}?path=${PATH}`,
 };
