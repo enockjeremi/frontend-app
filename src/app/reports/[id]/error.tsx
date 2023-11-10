fadc18e231ab044fee2df5d04a1e336dd64d39eb
Enock Jeremi <enockjeremi@gmail.com>
2023-11-09 22:37:44 -0300

    styled error page

--- src/app/reports/[id]/error.tsx
+++ src/app/reports/[id]/error.tsx
@@ -1,30 +1,25 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="grid place-items-center h-[80vh]">
+      <div>
+        <h1 className="text-xl text-center">Lo sentimos!</h1>
+        <span className="text-lg text-default-500">No se a podido encontrar la informacion que solicitaste.</span>
+      </div>
     </div>
-  )
+  );
 }
